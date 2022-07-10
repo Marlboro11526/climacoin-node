dcb914c1c0a77d7df7aa8703070dccde83079582
Marlboro11526 <RichardJohnston33@outlook.com>
2022-07-11 02:08:48 +0500

    Refactored the Colective pallet but there is some issues

--- runtime/src/configs/mod.rs
+++ runtime/src/configs/mod.rs
@@ -1,6 +1,7 @@
 pub mod authorship;
 pub mod bags_list;
-pub mod timestamp;
 pub mod balances;
+// pub mod collective;
+pub mod timestamp;
 pub mod transaction_payment;
 pub mod election_provider_multi_phase;
