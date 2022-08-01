c4bc04cff678b94b47688e819c12cb347199f0ea
Marlboro11526 <RichardJohnston33@outlook.com>
2022-08-01 20:19:45 +0530

    Updated inflation rate

--- runtime/src/configs/staking.rs
+++ runtime/src/configs/staking.rs
@@ -6,8 +6,8 @@ use pallet_treasury::BurnImbalanceAdapter;
 
 pallet_staking_reward_curve::build! {
 	const REWARD_CURVE: PiecewiseLinear<'static> = curve!(
-		min_inflation: 0_025_000,
-		max_inflation: 0_100_000,
+		min_inflation: 0_010_000,
+		max_inflation: 0_025_000,
 		ideal_stake: 0_100_000,
 		falloff: 0_050_000,
 		max_piece_count: 40,
@@ -24,8 +24,8 @@ impl pallet_staking::BenchmarkingConfig for StakingBenchmarkingConfig {
 parameter_types! {
 	// TODO: Update SessionsPerEra BondingDuration SlashDeferDuration For PROD
 	pub const SessionsPerEra: sp_staking::SessionIndex = 6;
-	pub const BondingDuration: sp_staking::EraIndex = 24 * 28;
-	pub const SlashDeferDuration: sp_staking::EraIndex = 24 * 7; // 1/4 the bonding duration.
+	pub const BondingDuration: sp_staking::EraIndex = 1 * 28;
+	pub const SlashDeferDuration: sp_staking::EraIndex = 1 * 7; // 1/4 the bonding duration.
 	pub const RewardCurve: &'static PiecewiseLinear<'static> = &REWARD_CURVE;
 	pub const MaxNominatorRewardedPerValidator: u32 = 256;
 	pub const OffendingValidatorsThreshold: Perbill = Perbill::from_percent(17);
