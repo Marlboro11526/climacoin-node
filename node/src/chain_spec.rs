d943998a27fa2d7011a03570641d66816a58aa47
Fenn Ignatius Saji <fennig1997@gmail.com>
2022-08-01 21:36:19 +0530

    Fixed supply division issue

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -258,7 +258,7 @@ fn testnet_genesis(
 		},
 		balances: BalancesConfig {
 			// Configure endowed accounts with initial balance of 1 << 60.
-			balances: endowed_accounts.iter().cloned().map(|k| (k, user_balance)).collect(),
+			balances: endowed_accounts.iter().cloned().map(|k| (k, each_user_balance)).collect(),
 		},
 		session: SessionConfig {
 			keys: initial_authorities
