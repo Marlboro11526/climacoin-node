6bfc851030b47b9ffe68f6074aeb55c36c15e97f
Fenn Ignatius Saji <fennig1997@gmail.com>
2022-08-08 22:52:57 +0530

    Changed staking config

--- runtime/src/configs/staking.rs
+++ runtime/src/configs/staking.rs
@@ -6,10 +6,10 @@ use pallet_treasury::BurnImbalanceAdapter;
 pallet_staking_reward_curve::build! {
 	const REWARD_CURVE: PiecewiseLinear<'static> = curve!(
 		min_inflation: 0_050_000,
-		max_inflation: 0_150_000,
+		max_inflation: 0_100_000,
 		ideal_stake: 0_500_000,
 		falloff: 0_050_000,
-		max_piece_count: 80,
+		max_piece_count: 40,
 		test_precision: 0_005_000,
 	);
 }
